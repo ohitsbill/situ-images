6ff62000e35ee34310016105ed70421d510a3819
Bill <bill@birostudios.com>
2022-06-17 08:05:04 +0100

    Add functionality to recognise image by ratio of width to height rather than specific dimensions

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import uuid
 
 UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/uploads')
 THIS_FOLDER = join(dirname(realpath(__file__)))
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -48,7 +48,7 @@ def download_file(name):
 
     flat = Image.open('static/uploads/' + name)
     width, height = flat.size
-    print(width, height)
+    ratio = float("{:.2f}".format(width/height))
 
     # Check which mockup is required
     # if "d48" in name:
@@ -63,18 +63,31 @@ def download_file(name):
     #     mockup = Image.open(os.path.join(app.config['THIS_FOLDER'], 'static/mockups/Superwide-Situ.jpg'))
     #     area = (285,334,1707,678)
     #     size = (1422, 344)
-    if width == 1600 and height == 800:
+    # if width == 1600 and height == 800:
+    #     mockup = Image.open(os.path.join(app.config['THIS_FOLDER'], 'static/mockups/Wide-Situ.jpg'))
+    #     area = (276,146,1670,843)
+    #     size = (1394, 697)
+    # elif width == 1078 and height == 1616:
+    #     mockup = Image.open(os.path.join(app.config['THIS_FOLDER'], 'static/mockups/Portrait-Situ.jpg'))
+    #     area = (699,99,1217,875)
+    #     size = (518, 776)
+    # elif width == 2981 and height == 721:
+    #     mockup = Image.open(os.path.join(app.config['THIS_FOLDER'], 'static/mockups/Superwide-Situ.jpg'))
+    #     area = (285,334,1707,678)
+    #     size = (1422, 344)
+    if ratio == 2.0:
         mockup = Image.open(os.path.join(app.config['THIS_FOLDER'], 'static/mockups/Wide-Situ.jpg'))
         area = (276,146,1670,843)
         size = (1394, 697)
-    elif width == 1078 and height == 1616:
+    elif ratio == 0.67:
         mockup = Image.open(os.path.join(app.config['THIS_FOLDER'], 'static/mockups/Portrait-Situ.jpg'))
         area = (699,99,1217,875)
         size = (518, 776)
-    elif width == 2981 and height == 721:
+    elif ratio == 4.13:
         mockup = Image.open(os.path.join(app.config['THIS_FOLDER'], 'static/mockups/Superwide-Situ.jpg'))
         area = (285,334,1707,678)
         size = (1422, 344)
+
 
     # Resize uploaded image to fit mockup
     flat.thumbnail(size)
